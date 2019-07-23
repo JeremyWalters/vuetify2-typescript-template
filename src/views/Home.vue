5c387c071e7ddc83a8022cd18cd4466a8c02ee0f
Jeremy Walters <jeremy@ordercloud.co.za>
2019-07-23 12:01:41 +0200

    chore: Update vuetify depenency

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,12 @@
 <template>
-  <HelloWorld />
+  <v-container fluid> </v-container>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import { Component, Vue, Watch, Prop } from "vue-property-decorator";
 import HelloWorld from "../components/HelloWorld.vue";
+import Axios from "axios";
 
-export default Vue.extend({
-  components: {
-    HelloWorld
-  }
-});
+@Component
+export default class Home extends Vue {}
 </script>
