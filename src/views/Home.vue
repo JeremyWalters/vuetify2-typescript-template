61925c77759951704de9b907fd61a113b6e6f19d
Jeremy Walters <jeremy@ordercloud.co.za>
2019-07-30 14:53:45 +0200

    style: run lint

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,49 @@
 <template>
-<v-container grid-list-xs>
-  <v-card class="mx-auto" v-if="weatherData" color="#F9F9F9" max-width="400">
-    <v-list-item two-line>
-      <v-list-item-content>
-        <v-list-item-title class="headline">{{weatherData.name}}</v-list-item-title>
-        <v-list-item-subtitle>Mon, 12:30 PM, Mostly sunny</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
+  <v-container grid-list-xs>
+    <v-card class="mx-auto" v-if="weatherData" color="#F9F9F9" max-width="400">
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title class="headline">{{
+            weatherData.name
+          }}</v-list-item-title>
+          <v-list-item-subtitle
+            >Mon, 12:30 PM, Mostly sunny</v-list-item-subtitle
+          >
+        </v-list-item-content>
+      </v-list-item>
 
-    <v-card-text>
-      <v-layout align-center>
-        <v-flex xs6 display-3>
-          {{weatherData.main.temp.toFixed()}}&deg;C
-        </v-flex>
-        <v-flex xs6>
-          <v-img
-            src="http://cdn.vuetifyjs.com/images/cards/sun.png"
-            alt="Sunny image"
-            width="92"
-          ></v-img>
-        </v-flex>
-      </v-layout>
-    </v-card-text>
+      <v-card-text>
+        <v-layout align-center>
+          <v-flex xs6 display-3>
+            {{ weatherData.main.temp.toFixed() }}&deg;C
+          </v-flex>
+          <v-flex xs6>
+            <v-img
+              src="http://cdn.vuetifyjs.com/images/cards/sun.png"
+              alt="Sunny image"
+              width="92"
+            ></v-img>
+          </v-flex>
+        </v-layout>
+      </v-card-text>
 
-    <v-list-item>
-      <v-list-item-icon>
-        <v-icon>mdi-send</v-icon>
-      </v-list-item-icon>
-      <v-list-item-subtitle>{{weatherData.wind.speed}} km/h</v-list-item-subtitle>
-    </v-list-item>
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-send</v-icon>
+        </v-list-item-icon>
+        <v-list-item-subtitle
+          >{{ weatherData.wind.speed }} km/h</v-list-item-subtitle
+        >
+      </v-list-item>
 
-    <v-list-item>
-      <v-list-item-icon>
-        <v-icon>mdi-cloud-download</v-icon>
-      </v-list-item-icon>
-      <v-list-item-subtitle>48%</v-list-item-subtitle>
-    </v-list-item>
-  </v-card>
-</v-container>
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-cloud-download</v-icon>
+        </v-list-item-icon>
+        <v-list-item-subtitle>48%</v-list-item-subtitle>
+      </v-list-item>
+    </v-card>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -78,7 +84,7 @@ interface Weather {
 @Component
 export default class Home extends Vue {
   // This will be a state member
-  private weatherData: Weather = {} as Weather ;
+  private weatherData: Weather = {} as Weather;
   async mounted() {
     // This would live in the vuex store actions
     const response = await Axios.get(
